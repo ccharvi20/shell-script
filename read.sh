bd92c3858a498c30d473bc038ffda6cae534ce0a
ccharvi20 <85384924+ccharvi20@users.noreply.github.com>
2024-09-19 09:20:46 +0530

    Usename & password

---
+++ read.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "Please enter your username"
+
+read USERNAME
+
+echo "username enetered is: $USERNAME"
+
+echo " Please eneter password"
+
+read password
+
+echo " Password is : $password"
