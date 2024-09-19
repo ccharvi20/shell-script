02e62852e5718318bb2a618c2ee2de81d821e032
ccharvi20 <85384924+ccharvi20@users.noreply.github.com>
2024-09-19 07:28:31 +0530

    ADD

---
+++ add.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+Number1=$1
+Number2=$2
+
+sum=$((Number1+Number2))
+
+echo "Addition of 2 numbers is "$sum"
--- variable.sh
+++ variable.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-Person1 = ccharvi
-Person2 = reddi
+Person1=ccharvi
+Person2=reddi
 echo "$Person1 : Namaste"
 echo "$Person2: Hyderabad"
